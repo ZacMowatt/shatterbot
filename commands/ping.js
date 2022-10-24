8253dee6240d4552ce06e514908bd279116467dd
Zac <rrp3140@autuni.ac.nz>
2022-10-24 19:28:14 +1300

    Add input for score metrics

--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-const description = "Send a pong message to check if the bot is online.";
-
-const init = (interaction, client) => {
-	interaction.reply("Pong!");
-};
-
-module.exports = { init, description };
